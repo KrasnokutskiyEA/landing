d1b9578cbf0c8f1ebf937195e311fa9c6624d9ed
krasnokutskiy.e <Krasnokutskiy.E@ostec-group.ru>
2021-04-22 03:26:18 +0300

    WIP

--- pages/index.vue
+++ pages/index.vue
@@ -5,19 +5,18 @@
     </h1>
 
     <img
-      src="~/assets/images/thumbnail.jpg"
-      data-src="https://res.cloudinary.com/dfsb9sgvt/image/upload/f_auto,q_auto/bkg_1_gfoads.jpg"
-      sizes="(max-width: 1800px) 100vw, 1800px"
+      src="~/assets/images/hero-thumb.svg"
       data-srcset="
-      https://res.cloudinary.com/dfsb9sgvt/image/upload/c_scale,w_320,f_auto,q_auto/bkg_1_gfoads.jpg 320w,
-      https://res.cloudinary.com/dfsb9sgvt/image/upload/c_scale,w_480,f_auto,q_auto/bkg_1_gfoads.jpg 480w,
-      https://res.cloudinary.com/dfsb9sgvt/image/upload/c_scale,w_768,f_auto,q_auto/bkg_1_gfoads.jpg 768w,
-      https://res.cloudinary.com/dfsb9sgvt/image/upload/c_scale,w_1024,f_auto,q_auto/bkg_1_gfoads.jpg 1024w,
-      https://res.cloudinary.com/dfsb9sgvt/image/upload/c_scale,w_1280,f_auto,q_auto/bkg_1_gfoads.jpg 1280w,
-      https://res.cloudinary.com/dfsb9sgvt/image/upload/c_scale,w_1800,f_auto,q_auto/bkg_1_gfoads.jpg 1800w,
+      https://res.cloudinary.com/dfsb9sgvt/image/upload/c_scale,w_320,f_auto,q_auto/hero.jpg 320w,
+      https://res.cloudinary.com/dfsb9sgvt/image/upload/c_scale,w_480,f_auto,q_auto/hero.jpg 480w,
+      https://res.cloudinary.com/dfsb9sgvt/image/upload/c_scale,w_768,f_auto,q_auto/hero.jpg 768w,
+      https://res.cloudinary.com/dfsb9sgvt/image/upload/c_scale,w_1024,f_auto,q_auto/hero.jpg 1024w,
+      https://res.cloudinary.com/dfsb9sgvt/image/upload/c_scale,w_1280,f_auto,q_auto/hero.jpg 1280w,
+      https://res.cloudinary.com/dfsb9sgvt/image/upload/c_scale,w_1800,f_auto,q_auto/hero.jpg 1800w,
       "
+      data-src="https://res.cloudinary.com/dfsb9sgvt/image/upload/f_auto,q_auto/hero.jpg"
       width="100%"
-      alt="An image example with Cloudinary"
+      alt="Hero image"
       class="lazyload"
     >
   </div>
