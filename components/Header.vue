705cc2e88b5cfcbb23b512f2ff2596a977c98037
krasnokutskiy.e <Krasnokutskiy.E@ostec-group.ru>
2021-05-29 05:14:31 +0300

    WIP

--- components/Header.vue
+++ components/Header.vue
@@ -18,32 +18,32 @@ export default {
 
   methods: {
     toggleMobileNav () {
-      this.mobileNav = !this.mobileNav
+      this.$set(this, 'mobileNav', !this.mobileNav)
     },
 
     checkScreen () {
       this.windowWidth = window.innerWidth
 
-      if (this.windowWidth <= 768) {
-        this.mobile = true
+      if (this.windowWidth <= 640) {
+        this.$set(this, 'mobile', true)
         return
       }
 
-      this.mobile = false
-      this.mobileNav = false
+      this.$set(this, 'mobile', false)
+      this.$set(this, 'mobileNav', false)
     }
   }
 }
 </script>
 
 <template>
-  <header :class="{'scrolled-nav': scrollPosition}" class="bg-green-400 z-50 w-screen fixed transition-all">
-    <nav class="flex-row px-12 py-0 relative">
+  <header :class="{'scrolled-nav': scrollPosition}" class="bg-green-400 z-30 w-screen fixed transition-all">
+    <nav class="flex flex-row px-12 py-2 relative justify-between">
       <div class="branding">
         <img src="@/assets/images/corona.svg" class="w-10" alt="">
       </div>
 
-      <ul v-show="!mobile" class="navigation">
+      <ul v-show="mobile === false" class="desktop-nav border flex flex-row items-center">
         <li>
           <NuxtLink to="/contacts">
             contacts
@@ -57,12 +57,17 @@ export default {
         </li>
       </ul>
 
-      <div class="icon absolute">
-        <div v-show="mobile" :class="{'icon-active': mobileNav }" class="w-5 h-5 bg-green-800" @click="toggleMobileNav" />
-      </div>
+      <!-- menu button burger -->
+      <MenuBtn
+        v-show="mobile"
+        :is-opened-menu="mobileNav"
+        class="absolute right-12 z-50"
+        @toggle-menu-button="toggleMobileNav"
+      />
+      <!-- -->
 
       <transition name="mobile-nav">
-        <ul v-show="mobileNav" class="dropdown-nav">
+        <ul v-if="mobileNav" class="dropdown-nav border absolute top-0 left-0 z-40 w-screen h-screen">
           <li>
             <NuxtLink to="/contacts">
               contacts
@@ -81,5 +86,13 @@ export default {
 </template>
 
 <style lang="scss" scoped>
+.mobile-nav-enter-active,
+.mobile-nav-leave-active {
+  transition: 1s ease all;
+}
 
+.mobile-nav-enter,
+.mobile-nav-leave-to {
+  transform: translateX(-100%);
+}
 </style>
