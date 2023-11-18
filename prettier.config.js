3e81cf2e8d6b9540fc495a889cb371f3e9a1243d
krasnokutskiy.e <Krasnokutskiy.E@ostec-group.ru>
2023-11-18 06:13:55 +0300

    project init

---
+++ prettier.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  plugins: ['prettier-plugin-tailwindcss'],
+  tailwindConfig: './tailwind.config.ts',
+  tailwindFunctions: ['clsx'],
+
+  /* rules */
+  singleQuote: true,
+  jsxSingleQuote: true,
+  semi: false,
+  trailingComma: 'none',
+}
